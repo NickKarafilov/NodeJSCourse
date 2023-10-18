149daff2348c4f4b7c8f10450b640fef3e86806f
nick <nrkjak@gmail.com>
2023-10-17 21:25:27 -0400

    NodeJS Commands

--- notesApp/app.js
+++ notesApp/app.js
@@ -1,15 +1,60 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const validator = require('validator')
-
-
-
 const getNotes = require('./notes.js')
 
-const allNotes = getNotes ('This is a ', 'Text')
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(chalk.blue.bold.inverse(allNotes))
+//create add command
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note')
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+       console.log('Removing the note') 
+    }
+})
 
 
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function () {
+        console.log('Listing the notes')
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: "Describe a note",
+    handler: function () {
+        console.log("Descibing a note")
+    }
+})
+
+console.log(yargs.argv)
+
+// Get Notes and change colour
+// 
+
+// const allNotes = getNotes ('This is a ', 'Text')
+
+// console.log(chalk.blue.bold.inverse(allNotes))
+
+// console.log(process.argv[2])
 
 
 
